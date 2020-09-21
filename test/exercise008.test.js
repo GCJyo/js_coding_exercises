550217678c724734bb5834c92c88ea42022e0f65
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-09-21 06:59:20 +0100

    Implemented test for Exercise008

--- test/exercise008.test.js
+++ test/exercise008.test.js
@@ -1,17 +1,16 @@
 
-const {tvRemote} = require("../challenges/exercise008.test");
-console.log(tvRemote);
+const {tvRemote} = require("../challenges/exercise008");
+//console.log(tvRemote);
 
-/*
 describe("tvRemote", () => {
   test("count of clicks", () => {
-    expect(tvRemote("codewars")).toBe("36");
-    expect(tvRemote("does")).toBe("16");
-    expect(tvRemote("your")).toBe("23");
-    expect(tvRemote("solution")).toBe("33");
-    expect(tvRemote("work")).toBe("20");
-    expect(tvRemote("for")).toBe("12");
-    expect(tvRemote("these")).toBe("27");
-    expect(tvRemote("words")).toBe("25");
+    expect(tvRemote("codewars")).toBe(36);
+    expect(tvRemote("does")).toBe(16);
+    expect(tvRemote("your")).toBe(23);
+    expect(tvRemote("solution")).toBe(33);
+    expect(tvRemote("work")).toBe(20);
+    expect(tvRemote("for")).toBe(12);
+    expect(tvRemote("these")).toBe(27);
+    expect(tvRemote("words")).toBe(25);
     });
-  });*/
+  });
