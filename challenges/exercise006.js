eb0cd247e9209b71902c8b6d90481cab28261915
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-10-18 12:54:57 +0100

    implemented exercise006 complementary DNA

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -20,25 +20,42 @@ const sumMultiples = arr => {
 };
 
 /**
- * This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.
+ * This function will receive a string of characters and should return true/false 
+ * depending on whether it is a valid DNA string. A valid DNA string may contain 
+ * characters C, G, T or A only.
  * @param {String} str
  * @returns {Boolean}
  */
 const isValidDNA = str => {
   if (str === undefined) throw new Error("str is required");
+  if (typeof(str) !== "string") throw new Error("str must be a String");
+  if (str.match(/(^[CGTA]*$)/)) {return true} else {return false};
 };
 
 /**
- * This function will receive a valid DNA string (see above) and should return a string of the complementary base pairs. In DNA, T always pairs with A, and C always pairs with G. So a string of "ACTG" would have a complementary DNA string of "TGAC".
+ * This function will receive a valid DNA string (see above) and should return 
+ * a string of the complementary base pairs. In DNA, T always pairs with A, and C 
+ * always pairs with G. So a string of "ACTG" would have a complementary DNA string
+ * of "TGAC".
  * @param {String} str
  * @returns {String}
  */
 const getComplementaryDNA = str => {
   if (str === undefined) throw new Error("str is required");
+  if (typeof(str) !== "string") throw new Error("must be a String");
+  if (str.match(/[^GCTA]/)) throw new Error("must have only CGTA");
+  return str.split("").map(char => {
+    if (char == "C") { return "G" }
+    if (char == "G") { return "C" }
+    if (char == "T") { return "A" }
+    if (char == "A") { return "T" }
+  }).join("")
 };
 
 /**
- * This function should receive a number and return true/false depending on whether it is a prime number or not. A prime number is a number that can only be divided evenly by 1 and itself (for example, 7)
+ * This function should receive a number and return true/false depending on 
+ * whether it is a prime number or not. A prime number is a number that can 
+ * only be divided evenly by 1 and itself (for example, 7)
  * @param {Number} n
  * @returns {Boolean}
  */
@@ -47,7 +64,9 @@ const isItPrime = n => {
 };
 
 /**
- * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
+ * This function should receive a number and return an array of n arrays, each 
+ * filled with n items. The parameter "fill" should be used as the filler of 
+ * the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
  * [
  *   ["foo", "foo", "foo"],
  *   ["foo", "foo", "foo"],
@@ -69,7 +88,9 @@ const createMatrix = (n, fill) => {
  *  { name: "Pedro", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
  *  ...etc
  * ]
- * and a day of the week. For the café to run successfully, at least 3 staff members are required per day. The function should return true/false depending on whether there are enough staff scheduled for the given day.
+ * and a day of the week. For the café to run successfully, at least 3 staff members 
+ * are required per day. The function should return true/false depending on whether
+ * there are enough staff scheduled for the given day.
  * @param {Array} staff
  * @param {String} day
  * @returns {Boolean}
--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -1,5 +1,5 @@
 const { TestScheduler } = require("jest");
-const {sumMultiples} = require ("../challenges/exercise006");
+const {sumMultiples, isValidDNA, getComplementaryDNA } = require ("../challenges/exercise006");
 describe("sumMultiples", () => {
     test("throws an error if not an array", () => {
         expect(()=>{
@@ -27,4 +27,50 @@ describe("sumMultiples", () => {
     test("returm 0 if there are no multiples of 3 or 5", () => {
         expect(sumMultiples([1,2,8,13,7])).toBe(0);
     });
+});
+describe("isValidDNA",() => {
+    test("check function requires a string", () => {
+        expect(() => {
+            isValidDNA()
+        }).toThrow("str is required")
+        expect(() => {
+            isValidDNA(2)
+        }).toThrow("str must be a String")
+        expect(() => {
+            isValidDNA(["GATA","CGTA"])
+        }).toThrow("str must be a String")
+    });
+    test("returns false if string has a character other than  C, G, T or A", () => {
+        expect(isValidDNA(" ")).toBe(false);
+        expect(isValidDNA("CGTA*!")).toBe(false);
+        expect(isValidDNA("cgta")).toBe(false);
+        expect(isValidDNA("CGTA ")).toBe(false);
+    });
+    test("returns true if string has only  C, G, T or A", () => {
+        expect(isValidDNA("G")).toBe(true);
+        expect(isValidDNA("CCTTAAGG")).toBe(true);
+        expect(isValidDNA("CGTACGTA")).toBe(true);
+    });
+});
+describe("getComplementaryDNA",() => {
+    // testing of the 'validDNA' function happens above
+    test("check that an invalid or empty DNA string returns false", () => {
+        expect(() => {
+            getComplementaryDNA("rtyt")
+        }).toThrow("must have only CGTA")
+        expect(() => {
+            getComplementaryDNA(8)
+        }).toThrow("must be a String")
+        expect(() => {
+            getComplementaryDNA(["GATA","CGTA"])
+        }).toThrow("must be a String")
+        expect(() => {
+            getComplementaryDNA()
+        }).toThrow("str is required")
+        });
+    test("check that a valid DNA string returns valid pairs", () => {
+        expect(getComplementaryDNA("ACTG")).toBe("TGAC")
+        expect(getComplementaryDNA("TGAC")).toBe("ACTG")
+        expect(getComplementaryDNA("TTT")).toBe("AAA")
+        });
 });
